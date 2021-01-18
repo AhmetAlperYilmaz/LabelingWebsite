8384e27221c17dd299b3daae33f8018c3824e357
Ahmet Alper Yılmaz <alperyilmaz2000@hotmail.com>
2021-01-18 21:59:58 +0300

    .

--- dbinit.py
+++ dbinit.py
@@ -76,7 +76,7 @@ def initialize(url):
 
 
 if __name__ == "__main__":
-    url = "dbname='LabelingDB' user='postgres' host='localhost' password='KingHarlaus26'"
+    url = os.getenv("DATABASE_URL")
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
